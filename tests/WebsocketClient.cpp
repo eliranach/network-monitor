7e404c08881d99c30a31723ab16c607edda09e84
unknown <elirannach@gmail.com>
2023-05-14 18:22:39 +0300

    Save cacert.pem file to tests folder

--- tests/WebsocketClient.cpp
+++ tests/WebsocketClient.cpp
@@ -1,5 +1,5 @@
 #include <network-monitor/WebsocketClient.h>
-
+#include <filesystem>
 #include <boost/asio.hpp>
 #include <boost/test/unit_test.hpp>
 
@@ -8,6 +8,11 @@
 using NetworkMonitor::WebSocketClient;
 
 BOOST_AUTO_TEST_SUITE(network_monitor);
+
+BOOST_AUTO_TEST_CASE(cacert_pem)
+{
+    BOOST_CHECK(std::filesystem::exists(TESTS_CACERT_PEM));
+}
 
 BOOST_AUTO_TEST_CASE(class_WebSocketClient)
 {
